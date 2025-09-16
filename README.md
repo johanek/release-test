6039d8459adb27ac038f72a006187903e6a804a7
Johan van den Dorpe <johan.vandendorpe@wifinity.co.uk>
2025-09-16 10:21:45 +0100

    test release-it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # release-test
+
+version: 0.0.1
---
+++ scripts/update_versions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import re
+from pathlib import Path
+
+if len(sys.argv) != 2:
+    print("Usage: update_versions.py <version>")
+    sys.exit(1)
+
+version = sys.argv[1]
+
+# update README.md
+readme_file = Path("README.md")
+readme_text = readme_file.read_text()
+
+readme_text_new = re.sub(
+  r"(version:)([0-9]+\.[0-9]+\.[0-9]+)",
+  rf"\1{version}",
+  readme_text
+)
+
+readme_file.write_text(readme_text_new)
+print(f"Updated {readme_file} to version {version}")
